518152c1541d74145a087bfc4933014af4ba9364
kanidrk <kanidrk1523@icloud.com>
2016-12-04 20:29:01 +0900

    プロジェクト ファイルを追加します。

---
+++ クイズジェネレーター/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace クイズジェネレーター
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter sw = new System.IO.StreamWriter("result.txt");
+
+            sw.WriteLine(textBox1.Text);
+            sw.WriteLine(textBox2.Text);
+            sw.WriteLine(textBox3.Text);
+            sw.WriteLine(textBox4.Text);
+            sw.WriteLine(textBox5.Text);
+            sw.WriteLine(textBox6.Text);
+            sw.WriteLine(textBox7.Text);
+            sw.WriteLine(textBox8.Text);
+            sw.Close();
+            System.Diagnostics.Process.Start("Notepad", "result.txt");
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
